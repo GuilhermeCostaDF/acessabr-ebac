065481d8eef750363c32a537c800e390ad165470
GuilhermeCostaDF <guido.costa@globo.com>
2021-11-23 20:27:22 -0300

    Criando a pagina Home e estilizando componente Pills

---
+++ src/pages/Home/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import './style.scss';
+import imagemDestaque from '../../images/imagem-destaque.png';
+import Pills from "../../components/Pills";
+
+const PLACES = [
+    'Praça',
+    'Parque',
+    'Igreja',
+    'Hotel',
+    'Restaurante',
+    'Loja',
+    'Zoológico'
+]
+
+
+const Home = () => {
+
+    const [selectedPill, setSelectedPill] = useState('');
+    return(
+
+        <main id="main-content" className="home__container">
+            <div className="home__col">
+                <h1 className="home__title"> São Paulo para todos</h1>
+                <p className="home__text">
+                    Nossa missão é facilitar o encontro entre lugares inclusivos e pessoas que buscam alternativas culturais para sair de casa, com a mobilidade que todo cidadão precisa.
+                </p>
+                <p className="home__text">
+                    Checamos 10 itens essenciais de acessibilidade para pessoas com deficiência em diversos pontos da cidade. 
+                </p>
+
+                <div className="home__pills">
+                    {
+                        PLACES.map((item) => (
+                            <Pills 
+                                local={item}
+                                selected={selectedPill === item}
+                                onClick={()=>setSelectedPill(item)}
+                             />
+                        ))
+                    }
+                </div>
+
+            </div>
+            <div className="home__col">
+                <div className="home__image--destaque">
+                    <img src={imagemDestaque} alt="" />
+                </div>
+            </div>
+        </main>
+
+    );
+
+}
+
+export default Home;
